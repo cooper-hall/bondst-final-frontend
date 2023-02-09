9d0e16121c9636d9689d355837833ea4572ea906
Cooper Hall <cooperhall111@gmail.com>
2023-02-08 19:46:07 -0500

    able to add prices to bottles

--- src/components/AlcModal.jsx
+++ src/components/AlcModal.jsx
@@ -1,48 +1,38 @@
+import {useState} from 'react'
+
 const AlcModal = ({setShowAlcModal, updateTicketList, addToTotal, currentBottle, setCurrentBottle}) => {
 
-    const addBrandToOrder = (bottle) => {
+    const [selectedOption, setSelectedOption] = useState(null)
+    const [price, setPrice] = useState(currentBottle.price)
+
+    const updatePrice = (newOption) => {
+        if (newOption === 'opt1') {
+          setPrice(currentBottle.price + 13);
+        } else if (newOption === 'opt2') {
+          setPrice(currentBottle.price + 9);
+        } else if (newOption === 'opt3') {
+          setPrice(currentBottle.price + 5);
+        } else if (newOption === 'opt4') {
+          setPrice(currentBottle.price);
+        }
+        setSelectedOption(newOption);
+    };
+
+     const addBrandToOrder = (bottle) => {
         updateTicketList(bottle)
-        addToTotal(bottle.price)
+        addToTotal(price)
         setShowAlcModal(false)
     }
-
-    const updatePrice = () => {
-        // let price = currentBottle.price + 0
-    //     // let option
-    //     //     id === "opt1" ? price = currentBottle.price + 13 :
-    //     //     id === "opt2" ? price = currentBottle.price + 9 :
-    //     //     id === "opt3" ? price = currentBottle.price + 5 :
-    //     //     id === "opt4" ? price = currentBottle.price :
-    //     // console.log(price)
-    //     // console.log(currentBottle)
-       
-    //     // console.log(setCurrentBottle.price)
-    //     switch(value){
-    //         case opt1:
-    //             return price = currentBottle.price + 13
-    //         case opt2:
-    //             return price = currentBottle.price + 9
-    //         case opt3: 
-    //             return price = currentBottle.price + 5
-    //         case opt4:
-    //             return price = currentBottle.price
-    //         default:
-    //             return currentBottle.price
-    //     }
-        // currentBottle.price = price 
-        // setCurrentBottle(currentBottle)
-    }
-
 
    return (
         <div className="alc-modal-container" > 
             <div className="alc-modal-content">
                 <div className="exit-button" onClick={() => setShowAlcModal(false)}>X</div>
-                <div className="alc-modal-brand"> How to serve: {currentBottle.name} ${currentBottle.price}</div>
-                    <h4 id="opt1" className="alc-modal-option" onClick={updatePrice()}> + house cocktail </h4>
-                    <h4 id="opt2" className="alc-modal-option" onClick={updatePrice()}> + classic cocktail </h4>
-                    <h4 id="opt3" className="alc-modal-option" onClick={updatePrice()}> + other drink </h4>
-                    <h4 id="opt4" className="alc-modal-option" onClick={updatePrice()}> + straight </h4>
+                <div className="alc-modal-brand"> How to serve: {currentBottle.name} ${price}</div>
+                    <h4 id="opt1" className={`alc-modal-option ${selectedOption === 'opt1' ? 'selected' : ''}`} onClick={()=> updatePrice('opt1')}> + house cocktail </h4>
+                    <h4 id="opt2" className={`alc-modal-option ${selectedOption === 'opt2' ? 'selected' : ''}`} onClick={()=> updatePrice('opt2')}> + classic cocktail </h4>
+                    <h4 id="opt3" className={`alc-modal-option ${selectedOption === 'opt3' ? 'selected' : ''}`} onClick={()=> updatePrice('opt3')}> + other drink </h4>
+                    <h4 id="opt4" className={`alc-modal-option ${selectedOption === 'opt4' ? 'selected' : ''}`} onClick={()=> updatePrice('opt4')}> + straight </h4>
                     <h4 className="alc-modal-option" onClick={()=>{addBrandToOrder(currentBottle)}}>submit</h4>
             </div>
         </div>
