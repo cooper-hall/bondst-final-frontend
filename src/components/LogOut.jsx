a5e193836d40474a09dea0c4c95303a6f78f87d1
Cooper Hall <cooperhall111@gmail.com>
2023-02-10 18:19:42 -0500

    fixed logout

--- src/components/LogOut.jsx
+++ src/components/LogOut.jsx
@@ -3,7 +3,7 @@ import {useNavigate} from 'react-router-dom'
 
 const LogOut = ({user, setUser}) => {
 
-  const navigate= useNavigate
+  const navigate= useNavigate()
 
   const logOut = () => {
     Cookies.remove('token')
