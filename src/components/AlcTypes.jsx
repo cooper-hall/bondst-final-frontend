79b49dd304ddd2085a5c6f5894b090cc038c737c
Cooper Hall <cooperhall111@gmail.com>
2023-02-21 12:38:24 -0500

    cleaned up some code

--- src/components/AlcTypes.jsx
+++ src/components/AlcTypes.jsx
@@ -22,63 +22,27 @@ const AlcTypes = ({addToTotal, updateTicketList, brands}) => {
           brands.slice(0, 16).map((bottle)=> {     
           let divcolor;
             switch (true) {
-    case bottle.quantity === 3 && bottle.alcType === "Gin":
+    case bottle.quantity === 3 :
       divcolor = "#df9a93";
       break;
-    case bottle.quantity === 2 && bottle.alcType === "Gin":
+    case bottle.quantity === 2 :
       divcolor = "#EA6D5F";
       break;
-    case bottle.quantity === 1 && bottle.alcType === "Gin":
+    case bottle.quantity === 1 :
       divcolor = "#DD1C1A";
       break;
-    case bottle.quantity === 0 && bottle.alcType === "Gin":
+    case bottle.quantity === 0 :
       divcolor = "#5B5B5B09";
       break; 
     case bottle.alcType === "Gin":
       divcolor = "#31519b";
       break;
-    case bottle.quantity === 3 && bottle.alcType === "Whiskey":
-      divcolor = "#df9a93";
-      break;
-    case bottle.quantity === 2 && bottle.alcType === "Whiskey":
-      divcolor = "#EA6D5F";
-      break;
-    case bottle.quantity === 1 && bottle.alcType === "Whiskey":
-      divcolor = "#DD1C1A";
-      break;
-    case bottle.quantity === 0 && bottle.alcType === "Whiskey":
-      divcolor = "#5B5B5B09";
-      break; 
     case bottle.alcType === "Whiskey":
       divcolor = "#778da9";
       break;
-    case bottle.quantity === 3 && bottle.alcType === "Vodka":
-      divcolor = "#df9a93";
-      break;
-    case bottle.quantity === 2 && bottle.alcType === "Vodka":
-      divcolor = "#EA6D5F";
-      break;
-    case bottle.quantity === 1 && bottle.alcType === "Vodka":
-      divcolor = "#DD1C1A";
-      break;
-     case bottle.quantity === 0 && bottle.alcType === "Vodka":
-      divcolor = "#5B5B5B09";
-      break; 
     case bottle.alcType === "Vodka":
       divcolor = "#415d43";
       break;
-    case bottle.quantity === 3 && bottle.alcType === "Tequila":
-      divcolor = "#df9a93";
-      break;
-    case bottle.quantity === 2 && bottle.alcType === "Tequila":
-      divcolor = "#EA6D5F";
-      break;
-    case bottle.quantity === 1 && bottle.alcType === "Tequila":
-      divcolor = "#DD1C1A";
-      break;
-    case bottle.quantity === 0 && bottle.alcType === "Tequila":
-      divcolor = "#5B5B5B09";
-      break; 
     case bottle.alcType === "Tequila":
       divcolor = "#83afa4";
       break;
