81f049eee6538de7d29666a2f2140530c689b7f3
Cooper Hall <cooperhall111@gmail.com>
2023-03-02 12:02:20 -0600

    added header to admin side on pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import Login from './components/Login'
 import MainPage from './components/MainPage'
 import AdminMain from './components/AdminSide/AdminMain'
+import AdminInventory from './components/AdminSide/AdminInventory'
+import AdminEmployees from './components/AdminSide/AdminEmployees'
+import CompanyTrends from './components/AdminSide/CompanyTrends'
 
 
 function App() {
@@ -16,10 +19,14 @@ function App() {
     <div className="main-page-container">
       <Routes>
         <Route path="/" element={<Login user={user} setUser={setUser} form={form}/>} />
-        <Route path="/admin" element={<AdminMain/>} />
         <Route path="/mainpage" element={<MainPage user={user} setUser={setUser}/>} />
+        <Route path="/admin" element={<AdminMain/>} />
+          
+          <Route path="/inventory" element={<AdminInventory/>} />
+          <Route path="/employees" element={<AdminEmployees/>} />
+          <Route path="/trends" element={<CompanyTrends/>} />
       </Routes>
-    </div>
+   </div>
   )
 }
 
--- src/components/AdminSide/AdminInventory.jsx
+++ src/components/AdminSide/AdminInventory.jsx
@@ -1,4 +1,16 @@
-const AdminInv = () => {
+import {useState, useEffect} from 'react'
+import AdminHeader from '../AdminSide/AdminHeader'
+
+
+const AdminInventory = () => {
+  return (
+    <div className="whole-page">
+      <div className="header">
+        <AdminHeader />
+      </div>
+      <div> INVENTORY</div>
+    </div>
+  )
 
 }
-export default AdminInv
+export default AdminInventory
