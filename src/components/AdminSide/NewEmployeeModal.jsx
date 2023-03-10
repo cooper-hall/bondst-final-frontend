5afb09bff3dc96e99f9091abcfbff42622fcba06
Cooper Hall <cooperhall111@gmail.com>
2023-03-10 10:42:43 -0500

    created new modal for admins to add new employees

---
+++ src/components/AdminSide/NewEmployeeModal.jsx
@@ -0,0 +1,13 @@
+const NewEmployeeModal = ({setShowModal}) => {
+  return (
+    <div className="alc-lookup-container">
+      <div className="alc-lookup-content">
+        <div className="">
+            New Employee Modal
+        </div>
+      </div>
+    </div>
+  )
+
+}
+export default NewEmployeeModal
